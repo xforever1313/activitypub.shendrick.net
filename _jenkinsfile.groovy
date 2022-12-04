a23a11272f97d0621eaa43b1f20255143bea3cde
Seth Hendrick <seth@shendrick.net>
2022-12-04 16:23:04 -0500

    sync current directory.

--- _jenkinsfile.groovy
+++ _jenkinsfile.groovy
@@ -81,7 +81,7 @@ pipeline
                     {
                         String verbose = "-v"; // Make "-v" for verbose mode.
                         String sshOptions = "-o BatchMode=yes -o StrictHostKeyChecking=accept-new -i \\\"${WEBSITE_KEY}\\\"";
-                        sh "cd ./checkout/_site && rsync --rsh=\"ssh ${verbose} ${sshOptions}\" -az --delete --exclude \".well-known\" ./_site ${ACTIVITYPUB_USER}@activitypub.shendrick.net:activitypub.shendrick.net";
+                        sh "cd ./checkout/_site && rsync --rsh=\"ssh ${verbose} ${sshOptions}\" -az --delete --exclude \".well-known\" ./ ${ACTIVITYPUB_USER}@activitypub.shendrick.net:activitypub.shendrick.net";
                     }
                 }
             }
